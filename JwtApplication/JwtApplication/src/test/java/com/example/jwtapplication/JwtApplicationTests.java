5705587f0134b09ac14e10f2fa793189eb06b093
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-04-25 22:24:17 +0530

    update

---
+++ JwtApplication/JwtApplication/src/test/java/com/example/jwtapplication/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- login/src/Users.java
+++
@@ -1,8 +0,0 @@
-public class Users {
-
-    public String name;
-    public String email;
-    public String address;
-    public String password;
-
-}
